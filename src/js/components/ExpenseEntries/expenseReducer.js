f2d2d61ec72f25683f89f63bc97f731fa33dc3fe
Larry Custodio <custodioly@gmail.com>
2017-11-03 14:40:57 -0700

    Adde root stores

---
+++ src/js/components/ExpenseEntries/expenseReducer.js
@@ -0,0 +1,10 @@
+const defaultState = {
+    description: '',
+    amount: '',
+    lineItems: []
+}
+
+export default function ExpenseReducer (state = defaultState, action) {
+    //state = defaultState acts as a default
+    return state;
+}
---
+++ src/js/components/IncomeEntries/incomeReducer.js
@@ -0,0 +1,10 @@
+const defaultState = {
+    description: '',
+    amount: '',
+    lineItems: []
+}
+
+export default function IncomeReducer(state = defaultState, action){
+    //state set to defaultstate unless specified
+    return state;
+}
