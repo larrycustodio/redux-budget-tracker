84a0182f0435d19cc9d409d5ad3ba90a7cf7632f
Larry Custodio <custodioly@gmail.com>
2017-12-10 23:29:34 -0800

    Fixed summary charts labels and ticks

--- src/js/components/Summary/SummaryChart.jsx
+++ src/js/components/Summary/SummaryChart.jsx
@@ -5,7 +5,8 @@ import {
     XAxis,
     YAxis,
     CartesianGrid,
-    Legend
+    Label,
+    LabelList
 } from 'recharts';
 
 export default class SummaryChart extends Component {
@@ -16,14 +17,21 @@ export default class SummaryChart extends Component {
             expenseTotal: parseFloat(this.props.expenseTotal)  },
         ];
         return (
-            <BarChart width={800} height={200} data={summaryData}
-                layout="vertical" barGap="7.5%">
+            <BarChart width={850} height={200} data={summaryData}
+                layout="vertical" barGap="12.5%">
                 <XAxis type="number"
-                    domain={[0, dataMax => ((dataMax * 1.1).toFixed(2))]} />
+                    domain={[0, dataMax => ((dataMax * 1.1).toFixed(2))]}>
+                    <Label value="Amount (USD)" offset={ 0 } position="insideBottom" />
+                </XAxis>
                 <YAxis type="category"
+                    tick={false}
                     dataKey="name" />
-                <Bar dataKey="incomeTotal" fill="#00d1b2" />
-                <Bar dataKey="expenseTotal" fill="#f57f97" />
+                <Bar dataKey="incomeTotal" fill="#00d1b2">
+                    <LabelList dataKey="incomeTotal" position="right" />
+                </Bar>
+                <Bar dataKey="expenseTotal" fill="#f57f97">
+                    <LabelList dataKey="expenseTotal" position="right" />
+                </Bar>
             </BarChart>
         );
     }
